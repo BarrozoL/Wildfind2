5b698f3e99e3524c398a040a88233850bbe07fd2
BarrozoL <barrozo.lucasp@gmail.com>
2025-11-02 10:41:17 +0000

    supabase integration

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -15,8 +15,9 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): Response|RedirectResponse
     {
-        return $request->user()->hasVerifiedEmail()
+        return redirect()->intended(route('dashboard', absolute: false));  //trying to bypass verify email redirect
+        /* return $request->user()->hasVerifiedEmail()
                     ? redirect()->intended(route('dashboard', absolute: false))
-                    : Inertia::render('auth/verify-email', ['status' => $request->session()->get('status')]);
+                    : Inertia::render('auth/verify-email', ['status' => $request->session()->get('status')]); */
     }
 }
---
+++ database/migrations/2025_10_31_120756_enable_postgis_pgtrgm.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+           if (DB::getDriverName() !== 'pgsql') {
+            return;
+        }
+
+/*         DB::statement('create extension if not exists postgis');
+        DB::statement('create extension if not exists pg_trgm'); */
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
